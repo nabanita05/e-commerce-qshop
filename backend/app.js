ffbe8ef24f2bcb9376342fed69bd70b36c8c89b2
nabanita05 <sarmaatinaban241i@gmail.com>
2023-12-26 21:39:29 +0530

    fixing bug 1 in payment

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ export const app = express();
 
 app.use(cors(
     //need to change the uri
-  {origin : ["https://qshop-frontend.vercel.app"],
+  {origin : ["https://qshop-frontend.vercel.app/paymentgateway"],
   methos : ["POST", "GET"],
   credentials : true,
 }
