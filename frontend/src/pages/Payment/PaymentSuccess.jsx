e0fdf9b142776a273aa93115f65ebf89b47208b6
nabanita05 <sarmaatinaban241i@gmail.com>
2023-12-27 21:45:26 +0530

    receipt generator added

--- frontend/src/pages/Payment/PaymentSuccess.jsx
+++ frontend/src/pages/Payment/PaymentSuccess.jsx
@@ -1,6 +1,29 @@
 
 import { useSearchParams } from "react-router-dom"
+import { useState } from "react";
+import axios from 'axios'
+import { saveAs } from 'file-saver';
+
 const PaymentSuccess = () => {
+
+    const [state, setState] = useState({
+        name: '',
+        receiptId: 0,
+        price1: 0,
+        price2: 0,
+    });
+    const handleChange = ({ target: { value, name } }) => {
+        setState((prevState) => ({ ...prevState, [name]: value }));
+    };
+    const createAndDownloadPdf = () => {
+        axios
+            .post('http://localhost:5000/create-pdf', state)
+            .then(() => axios.get('http://localhost:5000/fetch-pdf', { responseType: 'blob' }))
+            .then((res) => {
+                const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
+                saveAs(pdfBlob, 'newPdf.pdf');
+            });
+    };
 
     const seachQuery = useSearchParams()[0]
 
@@ -9,6 +32,13 @@ const PaymentSuccess = () => {
         <div>
             <h1>Order SUccesful</h1>
             <p>{referenceNum}</p>
+            <div className="App">
+                <input type="text" placeholder="Name" name="name" onChange={handleChange} />
+                <input type="number" placeholder="Receipt ID" name="receiptId" onChange={handleChange} />
+                <input type="number" placeholder="Price 1" name="price1" onChange={handleChange} />
+                <input type="number" placeholder="Price 2" name="price2" onChange={handleChange} />
+                <button onClick={createAndDownloadPdf}>Download PDF</button>
+            </div>
         </div>
     )
 }
