db5af3ec468538769cfe4ff0c760a7a9039762a8
nabanita05 <sarmaatinaban241i@gmail.com>
2023-12-28 12:35:07 +0530

    invoice generator added

--- receipt-server/documents/index.js
+++ receipt-server/documents/index.js
@@ -1,131 +1,171 @@
-module.exports = ({ name, price1, price2, receiptId }) => {
-    const today = new Date();
-return `
-    <!doctype html>
-    <html>
-       <head>
-          <meta charset="utf-8">
-          <title>PDF Result Template</title>
-          <style>
-             .invoice-box {
-             max-width: 800px;
-             margin: auto;
-             padding: 30px;
-             border: 1px solid #eee;
-             box-shadow: 0 0 10px rgba(0, 0, 0, .15);
-             font-size: 16px;
-             line-height: 24px;
-             font-family: 'Helvetica Neue', 'Helvetica',
-             color: #555;
-             }
-             .margin-top {
-             margin-top: 50px;
-             }
-             .justify-center {
-             text-align: center;
-             }
-             .invoice-box table {
-             width: 100%;
-             line-height: inherit;
-             text-align: left;
-             }
-             .invoice-box table td {
-             padding: 5px;
-             vertical-align: top;
-             }
-             .invoice-box table tr td:nth-child(2) {
-             text-align: right;
-             }
-             .invoice-box table tr.top table td {
-             padding-bottom: 20px;
-             }
-             .invoice-box table tr.top table td.title {
-             font-size: 45px;
-             line-height: 45px;
-             color: #333;
-             }
-             .invoice-box table tr.information table td {
-             padding-bottom: 40px;
-             }
-             .invoice-box table tr.heading td {
-             background: #eee;
-             border-bottom: 1px solid #ddd;
-             font-weight: bold;
-             }
-             .invoice-box table tr.details td {
-             padding-bottom: 20px;
-             }
-             .invoice-box table tr.item td {
-             border-bottom: 1px solid #eee;
-             }
-             .invoice-box table tr.item.last td {
-             border-bottom: none;
-             }
-             .invoice-box table tr.total td:nth-child(2) {
-             border-top: 2px solid #eee;
-             font-weight: bold;
-             }
-             @media only screen and (max-width: 600px) {
-             .invoice-box table tr.top table td {
-             width: 100%;
-             display: block;
-             text-align: center;
-             }
-             .invoice-box table tr.information table td {
-             width: 100%;
-             display: block;
-             text-align: center;
-             }
-             }
-          </style>
-       </head>
-       <body>
-          <div class="invoice-box">
-             <table cellpadding="0" cellspacing="0">
-                <tr class="top">
-                   <td colspan="2">
-                      <table>
-                         <tr>
-                            <td class="title"><img  src="https://i2.wp.com/cleverlogos.co/wp-content/uploads/2018/05/reciepthound_1.jpg?fit=800%2C600&ssl=1"
-                               style="width:100%; max-width:156px;"></td>
-                            <td>
-                               Datum: ${`${today.getDate()}. ${today.getMonth() + 1}. ${today.getFullYear()}.`}
-                            </td>
-                         </tr>
-                      </table>
-                   </td>
-                </tr>
-                <tr class="information">
-                   <td colspan="2">
-                      <table>
-                         <tr>
-                            <td>
-                               Customer name: ${name}
-                            </td>
-                            <td>
-                               Receipt number: ${receiptId}
-                            </td>
-                         </tr>
-                      </table>
-                   </td>
-                </tr>
-                <tr class="heading">
-                   <td>Bought items:</td>
-                   <td>Price</td>
-                </tr>
-                <tr class="item">
-                   <td>First item:</td>
-                   <td>${price1}$</td>
-                </tr>
-                <tr class="item">
-                   <td>Second item:</td>
-                   <td>${price2}$</td>
-                </tr>
-             </table>
-             <br />
-             <h1 class="justify-center">Total price: ${parseInt(price1) + parseInt(price2)}$</h1>
-          </div>
-       </body>
-    </html>
-    `;
+module.exports = ({ name, razorpay_order_id, razorpay_signature, razorpay_payment_id, price, shippingFee}) => {
+   const today = new Date();
+   const totalPrice = parseInt(price) + parseInt(shippingFee);
+
+   return `
+       <!doctype html>
+       <html>
+           <head>
+               <meta charset="utf-8">
+               <title>Modern Invoice</title>
+               <style>
+                   body {
+                       font-family: 'Arial', sans-serif;
+                       background-color: #f4f4f4;
+                       margin: 0;
+                       padding: 0;
+                   }
+
+                   .invoice-box {
+                       max-width: 800px;
+                       margin: 20px auto;
+                       background-color: #fff;
+                       box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+                       border: 1px solid #ccc;
+                       padding: 30px;
+                       font-size: 16px;
+                       line-height: 24px;
+                       color: #555;
+                       border-radius: 10px;
+                   }
+
+                   .invoice-box img {
+                       width: 100%;
+                       max-width: 200px;
+                       border-radius: 10px;
+                   }
+
+                   .invoice-box table {
+                       width: 100%;
+                       line-height: inherit;
+                       text-align: left;
+                       border-collapse: collapse;
+                   }
+
+                   .invoice-box table td {
+                       padding: 15px;
+                       vertical-align: top;
+                       border: 1px solid #ddd;
+                   }
+
+                   .invoice-box table tr.top table td {
+                       padding-bottom: 20px;
+                       background-color: #f8f8f8;
+                   }
+
+                   .invoice-box table tr.top table td.title {
+                       font-size: 45px;
+                       line-height: 45px;
+                       color: #333;
+                   }
+
+                   .invoice-box table tr.heading td {
+                       background: #eee;
+                       border-bottom: 1px solid #ddd;
+                       font-weight: bold;
+                   }
+
+                   .invoice-box table tr.details td {
+                       padding-bottom: 20px;
+                   }
+
+                   .invoice-box table tr.item td {
+                       border-bottom: 1px solid #eee;
+                       background-color: #f4f4f4;
+                   }
+
+                   .invoice-box table tr.item.last td {
+                       border-bottom: none;
+                   }
+
+                   .invoice-box table tr.total td:nth-child(2) {
+                       border-top: 2px solid #eee;
+                       font-weight: bold;
+                       background-color: #f8f8f8;
+                   }
+
+                   h1 {
+                       color: #555;
+                       text-align: right;
+                       margin-top: 20px;
+                   }
+
+                   @media only screen and (max-width: 600px) {
+                       .invoice-box table tr.top table td {
+                           width: 100%;
+                           display: block;
+                           text-align: center;
+                       }
+
+                       .invoice-box table tr.information table td {
+                           width: 100%;
+                           display: block;
+                           text-align: center;
+                       }
+                   }
+               </style>
+           </head>
+           <body>
+               <div class="invoice-box">
+                   <table cellpadding="0" cellspacing="0">
+                       <tr class="top">
+                           <td colspan="2">
+                               <table>
+                                   <tr>
+                                       <td class="title">
+                                           <img src="https://via.placeholder.com/200" alt="Logo">
+                                       </td>
+                                       <td>
+                                           Datum: ${`${today.getDate()}. ${today.getMonth() + 1}. ${today.getFullYear()}.`}
+                                       </td>
+                                   </tr>
+                               </table>
+                           </td>
+                       </tr>
+                       <tr class="information">
+                           <td colspan="2">
+                               <table>
+                                   <tr>
+                                       <td>
+                                           Customer Name: ${name}
+                                       </td>
+                                       <td>
+                                           Receipt number: ${razorpay_order_id}
+                                       </td>
+                                   </tr>
+                                   <tr>
+                                       <td>
+                                           Razorpay Order ID: ${razorpay_order_id}
+                                       </td>
+                                       <td>
+                                           Razorpay Payment ID: ${razorpay_payment_id}
+                                       </td>
+                                   </tr>
+                                   <tr>
+                                       <td>
+                                           Razorpay Signature: ${razorpay_signature}
+                                       </td>
+                                   </tr>
+                               </table>
+                           </td>
+                       </tr>
+                       <tr class="heading">
+                           <td>Item</td>
+                           <td>Price</td>
+                       </tr>
+                       <tr class="item">
+                           <td>Price:</td>
+                           <td>${price}$</td>
+                       </tr>
+                       <tr class="item">
+                           <td>Shipping Fee:</td>
+                           <td>${shippingFee}$</td>
+                       </tr>
+                   </table>
+                   <br />
+                   <h1>Total Price: ${totalPrice}$</h1>
+               </div>
+           </body>
+       </html>
+   `;
 };
